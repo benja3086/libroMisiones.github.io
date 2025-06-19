9da9c5b69eed9f41fd26db469799a0001c4af0ca
benja3086 <benjaminquinteros785@gmail.com>
2025-06-19 19:25:52 -0300

    arreglar error 2

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,6 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 export default defineConfig({
   base: '/libroMisiones.github.io/', // 👈 poné exactamente el nombre del repo de GitHub
   plugins: [react()],

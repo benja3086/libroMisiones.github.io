5969e974ea1aadabb6823d12215881ccc91d42e8
benja3086 <benjaminquinteros785@gmail.com>
2025-06-19 20:38:23 -0300

    Mis cambios antes del pull

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+
 export default defineConfig({
-  base: '/libroMisiones.github.io/', 
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
+  base: '/libroMisiones.github.io/', // 👈 MUY importante para GitHub Pages
 })

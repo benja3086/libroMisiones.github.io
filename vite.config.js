5b4f69f8a63ee712fbbc7c48afb03c6f16f2b24f
benja3086 <benjaminquinteros785@gmail.com>
2025-06-19 20:31:44 -0300

    prueba si anda con esto

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 export default defineConfig({
-  base: '/libroMisiones.github.io/', // 👈 poné exactamente el nombre del repo de GitHub
+  base: '/libroMisiones.github.io/', 
   plugins: [react()],
   resolve: {
     alias: {

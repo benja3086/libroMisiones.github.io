bed6ac8ff2c890d48555fd419afc237564aa1895
benja3086 <benjaminquinteros785@gmail.com>
2025-06-19 19:22:40 -0300

    arreglar otro error

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
-
 export default defineConfig({
+  base: '/libroMisiones.github.io/', // 👈 poné exactamente el nombre del repo de GitHub
   plugins: [react()],
   resolve: {
     alias: {
